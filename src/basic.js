e28057365bff228fcc2701afb5311e0c4e6a21a4
y0092888 <ju-horn@web.de>
2020-07-05 08:44:45 +0200

    refactored and no more need for info.json or header

--- src/basic.js
+++ src/basic.js
@@ -1,14 +1,40 @@
 let params = new URLSearchParams(location.search);
 const lunchType = params.get("meal");
 
-$.get("src/" + lunchType + "/info.json", function (data) {
-  const recepieCount = data.count;
-  for (let n = 1; n <= recepieCount; n++) {
-    $.get("src/" + lunchType + "/recepie" + n + "/info.json", function (data) {
-      document.getElementById("h" + n).innerHTML = data.header;
-      document.getElementById("p" + n).innerHTML = data.description;
-      document.getElementById("img" + n).src =
-        "src/" + lunchType + "/recepie" + n + "/img.jpg";
-    });
-  }
+console.log("hi");
+const ajaxFolder = "src/meals/" + lunchType + "/";
+
+let folders = [];
+
+$.ajax({
+  url: ajaxFolder,
+  success: function (data) {
+    console.log(data);
+    $(data)
+      .find("a:contains(/)") // is folder
+      .each(function () {
+        let folderName = urlToFolderName(this.href);
+        $.get("src/meals/" + lunchType + "/" + folderName + "/info.json").then(
+          (data) => {
+            data.folder = folderName;
+            addToHTML(1, data);
+          }
+        );
+      });
+  },
+  error: function (err, data) {
+    console.log(err);
+  },
 });
+
+function urlToFolderName(url) {
+  let hrefSplit = url.split("/");
+  return hrefSplit[hrefSplit.length - 2];
+}
+
+function addToHTML(index, data) {
+  document.getElementById("h" + index).innerHTML = data.folder;
+  document.getElementById("p" + index).innerHTML = data.description;
+  document.getElementById("img" + index).src =
+    "src/meals/" + lunchType + "/" + data.folder + "/img.jpg";
+}
